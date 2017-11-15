a4cb425130896fbaa92848d71ac0740515a35ab6
Ken Tran <kentran27@gmail.com>
2017-11-15 12:47:47 +0800

    Delete pg folder

--- timer/timer.py
+++ timer/timer.py
@@ -9,6 +9,6 @@ def get_breaks():
 path = 'https://www.youtube.com/watch?v=6P2nPI6CTlc'
 for i in range(get_breaks()):
     print('This start on {}'.format(time.ctime()))
-    time.sleep(3)
+    time.sleep(60*60)
     webbrowser.open(path)
     i+=1
